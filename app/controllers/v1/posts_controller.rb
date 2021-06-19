b51c453d9c0aad5cdfd98ecaf877422a2382dbe2
damari <motokin128@gmail.com>
2021-06-19 18:39:34 +0900

    create の API を実装して、テストを実行

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -10,6 +10,12 @@ class V1::PostsController < ApplicationController
   end
 
   def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -22,6 +28,10 @@ class V1::PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 end
